31389565cd4443f993d2fc52e559fcdc49fc61f4
Matthew Dove <matthew.dove@globalpayaunz.cloud>
2021-11-07 13:34:03 +1000

    Add clients for JSON, and XML; to be used for DI.

---
+++ src/FrameworkContainers/Format/JsonClient.cs
@@ -0,0 +1,48 @@
+﻿namespace FrameworkContainers.Format
+{
+    /// <summary>Dependency inversion alterative to the static class.</summary>
+    public interface IJsonClient
+    {
+        JsonMaybe Maybe { get; }
+        JsonResponse Response { get; }
+        T ToModel<T>(string json);
+        T ToModel<T>(string json, JsonOptions options);
+        string FromModel<T>(T model);
+        string FromModel<T>(T model, JsonOptions options);
+    }
+
+    public sealed class JsonClient : IJsonClient
+    {
+        public JsonMaybe Maybe => Json.Maybe;
+        public JsonResponse Response => Json.Response;
+
+        public T ToModel<T>(string json) => Json.ToModel<T>(json);
+        public T ToModel<T>(string json, JsonOptions options) => Json.ToModel<T>(json, options);
+
+        public string FromModel<T>(T model) =>  Json.FromModel(model);
+        public string FromModel<T>(T model, JsonOptions options) => Json.FromModel(model, options);
+    }
+
+    /// <summary>Dependency inversion alterative to the static class (when you are only using one type).</summary>
+    public interface IJsonClient<T>
+    {
+        JsonMaybe Maybe { get; }
+        JsonResponse Response { get; }
+        T ToModel(string json);
+        T ToModel(string json, JsonOptions options);
+        string FromModel(T model);
+        string FromModel(T model, JsonOptions options);
+    }
+
+    public sealed class JsonClient<T> : IJsonClient<T>
+    {
+        public JsonMaybe Maybe => Json.Maybe;
+        public JsonResponse Response => Json.Response;
+
+        public T ToModel(string json) => Json.ToModel<T>(json);
+        public T ToModel(string json, JsonOptions options) => Json.ToModel<T>(json, options);
+
+        public string FromModel(T model) => Json.FromModel(model);
+        public string FromModel(T model, JsonOptions options) => Json.FromModel(model, options);
+    }
+}
---
+++ src/FrameworkContainers/Format/XmlClient.cs
@@ -0,0 +1,44 @@
+﻿namespace FrameworkContainers.Format
+{
+    /// <summary>Dependency inversion alterative to the static class.</summary>
+    public interface IXmlClient
+    {
+        XmlMaybe Maybe { get; }
+        XmlResponse Response { get; }
+        T ToModel<T>(string xml);
+        string FromModel<T>(T model);
+        string FromModel<T>(T model, XmlOptions options);
+    }
+
+    public sealed class XmlClient : IXmlClient
+    {
+        public XmlMaybe Maybe => Xml.Maybe;
+        public XmlResponse Response => Xml.Response;
+
+        public T ToModel<T>(string xml) => Xml.ToModel<T>(xml);
+
+        public string FromModel<T>(T model) => Xml.FromModel(model);
+        public string FromModel<T>(T model, XmlOptions options) => Xml.FromModel(model, options);
+    }
+
+    /// <summary>Dependency inversion alterative to the static class (when you are only using one type).</summary>
+    public interface IXmlClient<T>
+    {
+        XmlMaybe Maybe { get; }
+        XmlResponse Response { get; }
+        T ToModel(string xml);
+        string FromModel(T model);
+        string FromModel(T model, XmlOptions options);
+    }
+
+    public sealed class XmlClient<T> : IXmlClient<T>
+    {
+        public XmlMaybe Maybe => Xml.Maybe;
+        public XmlResponse Response => Xml.Response;
+
+        public T ToModel(string xml) => Xml.ToModel<T>(xml);
+
+        public string FromModel(T model) => Xml.FromModel(model);
+        public string FromModel(T model, XmlOptions options) => Xml.FromModel(model, options);
+    }
+}
