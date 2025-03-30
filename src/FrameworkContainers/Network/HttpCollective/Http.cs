fde22deefe42a1802b9f5548115e47c5fb5a1909
Matthew Dove <matthew.dove@globalpay.com>
2025-03-31 00:02:18 +1000

    Updated nuget packages.

--- src/FrameworkContainers/Network/HttpCollective/Http.cs
+++ src/FrameworkContainers/Network/HttpCollective/Http.cs
@@ -14,13 +14,13 @@ public static class Http
     public static readonly HttpResponse Response = HttpResponse.Instance;
     public static readonly HttpMaybe Maybe = HttpMaybe.Instance;
 
-    private static HttpStatus IdentityValueStatus<E>(Task<Either<string, E>> t) where E : Exception => t.Result.Match(HttpStatus.Create, Identity<HttpStatus>);
+    private static HttpStatus IdentityValueStatus<E>(Task<Either<string, E>> t) where E : Exception => t.Result.Match(HttpStatus.Create, Throw<HttpStatus>);
 
-    private static HttpStatus IdentityValueStatus<E>(Task<Either<HttpStatus, E>> t) where E : Exception => t.Result.Match(Identity, Identity<HttpStatus>);
+    private static HttpStatus IdentityValueStatus<E>(Task<Either<HttpStatus, E>> t) where E : Exception => t.Result.Match(Identity, Throw<HttpStatus>);
 
-    private static HttpBody IdentityValueBody<E>(Task<Either<string, E>> t) where E : Exception => t.Result.Match(HttpBody.Create, Identity<HttpBody>);
+    private static HttpBody IdentityValueBody<E>(Task<Either<string, E>> t) where E : Exception => t.Result.Match(HttpBody.Create, Throw<HttpBody>);
 
-    private static T IdentityValue<T, E>(Task<Either<T, E>> t) where E : Exception => t.Result.Match(Identity, Identity<T>);
+    private static T IdentityValue<T, E>(Task<Either<T, E>> t) where E : Exception => t.Result.Match(Identity, Throw<T>);
 
     private static Func<string, T> Parse<T>(JsonOptions options) { return json => Json.ToModel<T>(json, options); }
 
@@ -33,7 +33,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send245(body, url.Match(static x => new Uri(x), Identity), contentType, options, headers, Constants.Http.POST)
-            .Match(Identity, Identity<Http245>);
+            .Match(Identity, Throw<Http245>);
     }
 
     public static HttpBody Post(string body, Either<string, Uri> url, string contentType, params Header[] headers)
@@ -45,7 +45,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(body, url.Match(static x => new Uri(x), Identity), contentType, options, headers, Constants.Http.POST)
-            .Match(HttpBody.Create, Identity<HttpBody>);
+            .Match(HttpBody.Create, Throw<HttpBody>);
     }
 
     public static HttpStatus PostStatus(string body, Either<string, Uri> url, string contentType, params Header[] headers)
@@ -57,7 +57,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(body, url.Match(static x => new Uri(x), Identity), contentType, new HttpOptions(options, retrieveHttpStatus: true), headers, Constants.Http.POST)
-            .Match(HttpStatus.Create, Identity<HttpStatus>);
+            .Match(HttpStatus.Create, Throw<HttpStatus>);
     }
 
     public static HttpStatus PostJsonStatus<TRequest>(TRequest model, Either<string, Uri> url, params Header[] headers)
@@ -69,7 +69,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(Json.FromModel(model, options), url.Match(static x => new Uri(x), Identity), Constants.Http.JSON_CONTENT, new HttpOptions(options, retrieveHttpStatus: true), headers, Constants.Http.POST)
-            .Match(HttpStatus.Create, Identity<HttpStatus>);
+            .Match(HttpStatus.Create, Throw<HttpStatus>);
     }
 
     public static TResponse PostJson<TRequest, TResponse>(TRequest model, Either<string, Uri> url, params Header[] headers)
@@ -81,7 +81,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(Json.FromModel(model, options), url.Match(static x => new Uri(x), Identity), Constants.Http.JSON_CONTENT, options, headers, Constants.Http.POST)
-            .Match(Parse<TResponse>(options), Identity<TResponse>);
+            .Match(Parse<TResponse>(options), Throw<TResponse>);
     }
 
     public static Http245 Put245(string body, Either<string, Uri> url, string contentType, params Header[] headers)
@@ -93,7 +93,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send245(body, url.Match(static x => new Uri(x), Identity), contentType, options, headers, Constants.Http.PUT)
-            .Match(Identity, Identity<Http245>);
+            .Match(Identity, Throw<Http245>);
     }
 
     public static HttpBody Put(string body, Either<string, Uri> url, string contentType, params Header[] headers)
@@ -105,7 +105,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(body, url.Match(static x => new Uri(x), Identity), contentType, options, headers, Constants.Http.PUT)
-            .Match(HttpBody.Create, Identity<HttpBody>);
+            .Match(HttpBody.Create, Throw<HttpBody>);
     }
 
     public static HttpStatus PutStatus(string body, Either<string, Uri> url, string contentType, params Header[] headers)
@@ -117,7 +117,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(body, url.Match(static x => new Uri(x), Identity), contentType, new HttpOptions(options, retrieveHttpStatus: true), headers, Constants.Http.PUT)
-            .Match(HttpStatus.Create, Identity<HttpStatus>);
+            .Match(HttpStatus.Create, Throw<HttpStatus>);
     }
 
     public static HttpStatus PutJsonStatus<TRequest>(TRequest model, Either<string, Uri> url, params Header[] headers)
@@ -129,7 +129,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(Json.FromModel(model, options), url.Match(static x => new Uri(x), Identity), Constants.Http.JSON_CONTENT, new HttpOptions(options, retrieveHttpStatus: true), headers, Constants.Http.PUT)
-            .Match(HttpStatus.Create, Identity<HttpStatus>);
+            .Match(HttpStatus.Create, Throw<HttpStatus>);
     }
 
     public static TResponse PutJson<TRequest, TResponse>(TRequest model, Either<string, Uri> url, params Header[] headers)
@@ -141,7 +141,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(Json.FromModel(model, options), url.Match(static x => new Uri(x), Identity), Constants.Http.JSON_CONTENT, options, headers, Constants.Http.PUT)
-            .Match(Parse<TResponse>(options), Identity<TResponse>);
+            .Match(Parse<TResponse>(options), Throw<TResponse>);
     }
 
     public static Http245 Patch245(string body, Either<string, Uri> url, string contentType, params Header[] headers)
@@ -153,7 +153,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send245(body, url.Match(static x => new Uri(x), Identity), contentType, options, headers, Constants.Http.PATCH)
-            .Match(Identity, Identity<Http245>);
+            .Match(Identity, Throw<Http245>);
     }
 
     public static HttpBody Patch(string body, Either<string, Uri> url, string contentType, params Header[] headers)
@@ -165,7 +165,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(body, url.Match(static x => new Uri(x), Identity), contentType, options, headers, Constants.Http.PATCH)
-            .Match(HttpBody.Create, Identity<HttpBody>);
+            .Match(HttpBody.Create, Throw<HttpBody>);
     }
 
     public static HttpStatus PatchStatus(string body, Either<string, Uri> url, string contentType, params Header[] headers)
@@ -177,7 +177,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(body, url.Match(static x => new Uri(x), Identity), contentType, new HttpOptions(options, retrieveHttpStatus: true), headers, Constants.Http.PATCH)
-            .Match(HttpStatus.Create, Identity<HttpStatus>);
+            .Match(HttpStatus.Create, Throw<HttpStatus>);
     }
 
     public static HttpStatus PatchJsonStatus<TRequest>(TRequest model, Either<string, Uri> url, params Header[] headers)
@@ -189,7 +189,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(Json.FromModel(model, options), url.Match(static x => new Uri(x), Identity), Constants.Http.JSON_CONTENT, new HttpOptions(options, retrieveHttpStatus: true), headers, Constants.Http.PATCH)
-            .Match(HttpStatus.Create, Identity<HttpStatus>);
+            .Match(HttpStatus.Create, Throw<HttpStatus>);
     }
 
     public static TResponse PatchJson<TRequest, TResponse>(TRequest model, Either<string, Uri> url, params Header[] headers)
@@ -201,7 +201,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(Json.FromModel(model, options), url.Match(static x => new Uri(x), Identity), Constants.Http.JSON_CONTENT, options, headers, Constants.Http.PATCH)
-            .Match(Parse<TResponse>(options), Identity<TResponse>);
+            .Match(Parse<TResponse>(options), Throw<TResponse>);
     }
 
     public static Http245 Get245(Either<string, Uri> url, params Header[] headers)
@@ -213,7 +213,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send245(string.Empty, url.Match(static x => new Uri(x), Identity), string.Empty, options, headers, Constants.Http.GET)
-            .Match(Identity, Identity<Http245>);
+            .Match(Identity, Throw<Http245>);
     }
 
     public static HttpBody Get(Either<string, Uri> url, params Header[] headers)
@@ -225,7 +225,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(string.Empty, url.Match(static x => new Uri(x), Identity), string.Empty, options, headers, Constants.Http.GET)
-            .Match(HttpBody.Create, Identity<HttpBody>);
+            .Match(HttpBody.Create, Throw<HttpBody>);
     }
 
     public static HttpStatus GetStatus(Either<string, Uri> url, params Header[] headers)
@@ -237,7 +237,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(string.Empty, url.Match(static x => new Uri(x), Identity), string.Empty, new HttpOptions(options, retrieveHttpStatus: true), headers, Constants.Http.GET)
-            .Match(HttpStatus.Create, Identity<HttpStatus>);
+            .Match(HttpStatus.Create, Throw<HttpStatus>);
     }
 
     public static TResponse GetJson<TResponse>(Either<string, Uri> url, params Header[] headers)
@@ -249,7 +249,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(string.Empty, url.Match(static x => new Uri(x), Identity), string.Empty, options, headers, Constants.Http.GET)
-            .Match(Parse<TResponse>(options), Identity<TResponse>);
+            .Match(Parse<TResponse>(options), Throw<TResponse>);
     }
 
     public static HttpStatus DeleteStatus(Either<string, Uri> url, params Header[] headers)
@@ -261,7 +261,7 @@ public static class Http
     {
         return HypertextTransferProtocol
             .Send(string.Empty, url.Match(static x => new Uri(x), Identity), string.Empty, new HttpOptions(options, retrieveHttpStatus: true), headers, Constants.Http.DELETE)
-            .Match(HttpStatus.Create, Identity<HttpStatus>);
+            .Match(HttpStatus.Create, Throw<HttpStatus>);
     }
 
     public static Task<Http245> Post245Async(string body, Either<string, Uri> url, string contentType, params Header[] headers)
